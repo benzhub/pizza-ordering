7e284c97d0cd75c040c59ed10761ce13b755bade
Len Rich <lenrich1403@gmail.com>
2024-02-24 23:46:15 +0800

    CartPage Skeleton

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,27 +1,35 @@
 "use client";
+import { Skeleton } from "@/app/components";
 import {
   CartItem as CartItemType,
   clearCart,
   getCarts,
 } from "@/lib/features/cart/cartsSlice";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
-import { Button, Container, Flex } from "@radix-ui/themes";
+import { Box, Button, Container, Flex } from "@radix-ui/themes";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import CartEmpty from "./_components/CartEmpty";
 import { CartItem } from "./_components/CartItem";
+import { SkeletonTheme } from "react-loading-skeleton";
 
 const CartPage = () => {
   const dispatch = useAppDispatch();
+  const [isLoading, setIsLoading] = useState(true);
   const [cartItems, setCartItems] = useState<CartItemType[]>([]);
   const carts = useAppSelector(getCarts);
 
   useEffect(() => {
     setCartItems(carts);
+    setIsLoading(false);
   }, [carts]);
 
   function handleClearCart() {
     dispatch(clearCart());
+  }
+
+  if (isLoading) {
+    return <CartPageSkeleton />;
   }
   return (
     <Container>
@@ -54,6 +62,91 @@ const CartPage = () => {
           </Flex>
         )}
       </div>
+    </Container>
+  );
+};
+
+const CartPageSkeleton = () => {
+  return (
+    <Container className="p-4">
+      <SkeletonTheme baseColor="#e2e8f0" highlightColor="#f8fafc">
+        <Box className="flex flex-col gap-2 divide-y divide-solid">
+          <div className="flex flex-col lg:flex-row lg:justify-between gap-4 py-2">
+            <Box className="w-[50%] lg:w-[25%]">
+              <Skeleton height="3rem" />
+            </Box>
+            <Box className="flex items-center justify-between w-full lg:w-[25%]">
+              <Box className="w-[25%]">
+                <Skeleton height="3rem" style={{ borderRadius: "50px" }} />
+              </Box>
+              <Box className="w-[25%]">
+                <Skeleton height="3rem" style={{ borderRadius: "50px" }} />
+              </Box>
+              <Box className="w-[25%]">
+                <Skeleton height="3rem" style={{ borderRadius: "50px" }} />
+              </Box>
+            </Box>
+          </div>
+          <div className="flex flex-col lg:flex-row lg:justify-between gap-4 py-2">
+            <Box className="w-[50%] lg:w-[25%]">
+              <Skeleton height="3rem" />
+            </Box>
+            <Box className="flex items-center justify-between w-full lg:w-[25%]">
+              <Box className="w-[25%]">
+                <Skeleton height="3rem" style={{ borderRadius: "50px" }} />
+              </Box>
+              <Box className="w-[25%]">
+                <Skeleton height="3rem" style={{ borderRadius: "50px" }} />
+              </Box>
+              <Box className="w-[25%]">
+                <Skeleton height="3rem" style={{ borderRadius: "50px" }} />
+              </Box>
+            </Box>
+          </div>
+          <div className="flex flex-col lg:flex-row lg:justify-between gap-4 py-2">
+            <Box className="w-[50%] lg:w-[25%]">
+              <Skeleton height="3rem" />
+            </Box>
+            <Box className="flex items-center justify-between w-full lg:w-[25%]">
+              <Box className="w-[25%]">
+                <Skeleton height="3rem" style={{ borderRadius: "50px" }} />
+              </Box>
+              <Box className="w-[25%]">
+                <Skeleton height="3rem" style={{ borderRadius: "50px" }} />
+              </Box>
+              <Box className="w-[25%]">
+                <Skeleton height="3rem" style={{ borderRadius: "50px" }} />
+              </Box>
+            </Box>
+          </div>
+          <div className="flex flex-col lg:flex-row lg:justify-between gap-4 py-2">
+            <Box className="w-[50%] lg:w-[25%]">
+              <Skeleton height="3rem" />
+            </Box>
+            <Box className="flex items-center justify-between w-full lg:w-[25%]">
+              <Box className="w-[25%]">
+                <Skeleton height="3rem" style={{ borderRadius: "50px" }} />
+              </Box>
+              <Box className="w-[25%]">
+                <Skeleton height="3rem" style={{ borderRadius: "50px" }} />
+              </Box>
+              <Box className="w-[25%]">
+                <Skeleton height="3rem" style={{ borderRadius: "50px" }} />
+              </Box>
+            </Box>
+          </div>
+          <Box className="py-2">
+            <Flex justify="start" gap="4">
+              <Box className="w-[25%] lg:w-[20%]">
+                <Skeleton height="3rem" style={{ borderRadius: "50px" }} />
+              </Box>
+              <Box className="w-[25%] lg:w-[20%]">
+                <Skeleton height="3rem" style={{ borderRadius: "50px" }} />
+              </Box>
+            </Flex>
+          </Box>
+        </Box>
+      </SkeletonTheme>
     </Container>
   );
 };
