5efb7e94a1066ece18d13152f12671e7ccd7a30d
Len Rich <lenrich1403@gmail.com>
2024-02-28 16:53:50 +0800

    Product Component add Pagination

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,13 +1,25 @@
-import { Container, Heading } from "@radix-ui/themes";
+"use client";
+import { Box, Container, Heading } from "@radix-ui/themes";
 import ProductList from "./_components/ProductList";
+import Pagination from "./Pagination";
+import { useProducts } from "./_components/useProducts";
+import PAGESIZE from "@/utils/pageSize";
 
-const ProductPage = () => {
+const ProductPage = ({ searchParams }: { searchParams: { page: string } }) => {
+  const page = parseInt(searchParams.page) || 1;
+  const { pizzas = [], totalCount, error, isLoading } = useProducts(page);
+
   return (
     <Container className="p-4">
-      <Heading as="h3">
-        Products List
-      </Heading>
-      <ProductList />
+      <Heading as="h3">Products List</Heading>
+      <ProductList pizzas={pizzas} error={error} isLoading={isLoading} />
+      <Box className="flex justify-center py-6">
+        <Pagination
+          itemCount={totalCount}
+          pageSize={PAGESIZE}
+          currentPage={page}
+        />
+      </Box>
     </Container>
   );
 };
