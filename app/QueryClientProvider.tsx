f47ed4b504293197087e5f3ea03943e44a71e004
Len Rich <lenrich1403@gmail.com>
2024-03-05 16:51:11 +0800

    React Query Hydration

--- app/QueryClientProvider.tsx
+++ app/QueryClientProvider.tsx
@@ -3,13 +3,26 @@ import {
   QueryClient,
   QueryClientProvider as ReactQueryClientProvider,
 } from "@tanstack/react-query";
-import { PropsWithChildren } from "react";
-
-const queryClient = new QueryClient();
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { PropsWithChildren, useState } from "react";
 
 const QueryClientProvider = ({ children }: PropsWithChildren) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: 60 * 1000,
+            retry: 3,
+          },
+        },
+      })
+  );
   return (
     <ReactQueryClientProvider client={queryClient}>
+      {/* <ReactQueryDevtools initialIsOpen={true} /> */}
       {children}
     </ReactQueryClientProvider>
   );
